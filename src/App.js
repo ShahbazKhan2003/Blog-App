8a8c7c5f5085360f83e77e4d60c8d8528dc8900a
ShahbazKhan2003 <shahbazkhan082003@gmail.com>
2024-06-04 11:55:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Layout from './Layout';
+import Post from "./Post";
+import { Routes, Route } from "react-router-dom";
+import IndexPage from './pages/IndexPage';
+import Loginpage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import { UserContext, UserContextProvider } from './UserContext';
+import CreatePost from './pages/CreatePost';
+import PostPage from './pages/PostPage';
+import EditPost from './pages/Editpost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage/>} />
+        <Route path='/login' element={<Loginpage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/create' element={<CreatePost/>} />
+        <Route path='/post/:id' element={<PostPage/>} />
+        <Route path='/edit/:id' element={<EditPost/>} />
+      </Route>
+
+    </Routes>
+    </UserContextProvider>
   );
 }
 
